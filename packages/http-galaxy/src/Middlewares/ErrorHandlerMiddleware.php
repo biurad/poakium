a8369b5f4639a89700aa89c535e27ec8ddbd071f
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-09-30 16:27:46 +0000

    Updated middleware for handling 4XX+ status code

--- packages/http-galaxy/src/Middlewares/ErrorHandlerMiddleware.php
+++ packages/http-galaxy/src/Middlewares/ErrorHandlerMiddleware.php
@@ -17,42 +17,21 @@ declare(strict_types=1);
 
 namespace Biurad\Http\Middlewares;
 
-use Biurad\Http\Exceptions\ClientException;
-use Biurad\Http\Exceptions\ClientExceptions\AccessDeniedException;
-use Biurad\Http\Exceptions\ClientExceptions\BadRequestException;
-use Biurad\Http\Exceptions\ClientExceptions\ConflictException;
-use Biurad\Http\Exceptions\ClientExceptions\GoneException;
-use Biurad\Http\Exceptions\ClientExceptions\LengthRequiredException;
-use Biurad\Http\Exceptions\ClientExceptions\MethodNotAllowedException;
-use Biurad\Http\Exceptions\ClientExceptions\NotAcceptableException;
-use Biurad\Http\Exceptions\ClientExceptions\NotFoundException;
-use Biurad\Http\Exceptions\ClientExceptions\PreconditionFailedException;
-use Biurad\Http\Exceptions\ClientExceptions\PreconditionRequiredException;
-use Biurad\Http\Exceptions\ClientExceptions\ServerErrorException;
-use Biurad\Http\Exceptions\ClientExceptions\ServiceUnavailableException;
-use Biurad\Http\Exceptions\ClientExceptions\TooManyRequestsException;
-use Biurad\Http\Exceptions\ClientExceptions\UnauthorizedException;
-use Biurad\Http\Exceptions\ClientExceptions\UnprocessableEntityException;
-use Biurad\Http\Exceptions\ClientExceptions\UnsupportedMediaTypeException;
+use GuzzleHttp\Exception\RequestException;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
 /**
- * Default response errorhandler middleware.
+ * Throw exception when the response of a request is not acceptable.
  *
- * Wraps Client and Routing exceptions into proper response.
+ * Status codes 400-499 lead to a ClientErrorException, status 500-599 to a ServerErrorException.
  */
-class ErrorHandlerMiddleware implements MiddlewareInterface
+final class ErrorHandlerMiddleware implements MiddlewareInterface
 {
     /**
      * {@inheritDoc}
-     *
-     * @param Request        $request
-     * @param RequestHandler $handler
-     *
-     * @return ResponseInterface
      */
     public function process(Request $request, RequestHandler $handler): ResponseInterface
     {
@@ -70,105 +49,12 @@ class ErrorHandlerMiddleware implements MiddlewareInterface
                 ->withoutHeader('Content-Length');
         }
 
-        // Handle Headers Error
-        if ($response->getStatusCode() >= 400) {
-            for ($i = 400; $i < 600; $i++) {
-                switch ($i) {
-                    case 400:
-                        $exception = new BadRequestException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 401:
-                        $exception = new UnauthorizedException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 403:
-                        $exception = new AccessDeniedException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 404:
-                        throw new NotFoundException();
-
-                    case 405:
-                        throw new MethodNotAllowedException();
-
-                    case 406:
-                        $exception = new NotAcceptableException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 409:
-                        $exception = new ConflictException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 410:
-                        $exception = new GoneException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 411:
-                        $exception = new LengthRequiredException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 412:
-                        $exception = new PreconditionFailedException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 415:
-                        $exception = new UnsupportedMediaTypeException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 422:
-                        $exception = new UnprocessableEntityException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 428:
-                        $exception = new PreconditionRequiredException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 429:
-                        $exception = new TooManyRequestsException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    case 500:
-                        throw new ServerErrorException();
-
-                    case 503:
-                        $exception = new ServiceUnavailableException();
-                        $exception->withResponse($response);
-
-                        throw $exception;
-
-                    default:
-                        throw new ClientException($i);
-                }
-            }
+        // If is below 4XX Responses code will never throw an exception
+        if ($response->getStatusCode() < 400) {
+            return $response;
         }
 
-        // remove headers that MUST NOT be included with 304 Not Modified responses
-        return $response;
+        throw RequestException::create($request, $response);
     }
 
     /**
