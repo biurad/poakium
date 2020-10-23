3097f53ac06c1eeaac78fdce303a2b337e8e206b
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-10-23 00:58:01 +0000

    Added twig and latte template renders

---
+++ packages/templating/src/Renders/LatteRender.php
@@ -0,0 +1,157 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Biurad opensource projects.
+ *
+ * PHP version 7.2 and above required
+ *
+ * @author    Divine Niiquaye Ibok <divineibok@gmail.com>
+ * @copyright 2019 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Biurad\UI\Renders;
+
+use Latte;
+use Latte\Loaders\StringLoader;
+use Nette;
+
+final class LatteRender extends AbstractRender
+{
+    protected const EXTENSIONS = ['latte'];
+
+    /** @var Latte\Engine */
+    protected $latte;
+
+    /**
+     * LatteEngine constructor.
+     *
+     * @param Latte\Engine $engine
+     * @param string[]     $extensions
+     */
+    public function __construct(Latte\Engine $engine, array $extensions = self::EXTENSIONS)
+    {
+        $this->latte      = $engine;
+        $this->extensions = $extensions;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render(string $template, array $parameters): string
+    {
+        $parameters = \array_merge($parameters, ['view' => $this]);
+        $source     = $this->getLoader()->find($template);
+
+        $this->latte->setLoader(new StringLoader([
+            $template => (string) $source->isFile() ? \file_get_contents($source) : $source,
+        ]));
+
+        return $this->latte->renderToString($template, $parameters);
+    }
+
+    /**
+     * Registers run-time filter.
+     *
+     * @param null|string $name
+     * @param callable    $callback
+     *
+     * @return static
+     */
+    public function addFilter(?string $name, callable $callback)
+    {
+        $this->latte->addFilter($name, $callback);
+
+        return $this;
+    }
+
+    /**
+     * Adds new macro.
+     *
+     * @param string      $name
+     * @param Latte\Macro $macro
+     *
+     * @return static
+     */
+    public function addMacro(string $name, Latte\Macro $macro)
+    {
+        $this->latte->addMacro($name, $macro);
+
+        return $this;
+    }
+
+    /**
+     * Registers run-time function.
+     *
+     * @param string   $name
+     * @param callable $callback
+     *
+     * @return static
+     */
+    public function addFunction(string $name, callable $callback)
+    {
+        $this->latte->addFunction($name, $callback);
+
+        return $this;
+    }
+
+    /**
+     * Sets translate adapter.
+     *
+     * @param null|Nette\Localization\ITranslator $translator
+     *
+     * @return static
+     */
+    public function setTranslator(?Nette\Localization\ITranslator $translator)
+    {
+        $this->latte->addFilter('translate', function (Latte\Runtime\FilterInfo $fi, ...$args) use ($translator): string {
+            return $translator === null ? $args[0] : $translator->translate(...$args);
+        });
+
+        return $this;
+    }
+
+    /**
+     * Adds new provider.
+     *
+     * @param string $name
+     * @param mixed  $value
+     *
+     * @return static
+     */
+    public function addProvider(string $name, $value)
+    {
+        $this->latte->addProvider($name, $value);
+
+        return $this;
+    }
+
+    /**
+     * @param null|Latte\Policy $policy
+     *
+     * @return static
+     */
+    public function setPolicy(?Latte\Policy $policy)
+    {
+        $this->latte->setPolicy($policy);
+
+        return $this;
+    }
+
+    /**
+     * @param callable $callback
+     *
+     * @return static
+     */
+    public function setExceptionHandler(callable $callback)
+    {
+        $this->latte->setExceptionHandler($callback);
+
+        return $this;
+    }
+}
---
+++ packages/templating/src/Renders/TwigRender.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Biurad opensource projects.
+ *
+ * PHP version 7.2 and above required
+ *
+ * @author    Divine Niiquaye Ibok <divineibok@gmail.com>
+ * @copyright 2019 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Biurad\UI\Renders;
+
+use Twig;
+use Twig\Cache\CacheInterface;
+use Twig\Extension\ExtensionInterface;
+use Twig\Loader\ArrayLoader;
+use Twig\Loader\ChainLoader;
+use Twig\NodeVisitor\NodeVisitorInterface;
+use Twig\RuntimeLoader\RuntimeLoaderInterface;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+final class TwigRender extends AbstractRender
+{
+    protected const EXTENSIONS = ['twig'];
+
+    /** @var Twig\Environment */
+    protected $environment;
+
+    /**
+     * TwigEngine constructor.
+     *
+     * @param Twig\Environment $engine
+     * @param string[]         $extensions
+     */
+    public function __construct(Twig\Environment $environment, array $extensions = self::EXTENSIONS)
+    {
+        $this->environment = $environment;
+        $this->extensions  = $extensions;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render(string $template, array $parameters): string
+    {
+        $parameters = \array_merge($parameters, ['view' => $this]);
+        $source     = $this->getLoader()->find($template);
+
+        $this->environment->setLoader(new ChainLoader([
+            new ArrayLoader([
+                $template => (string) $source->isFile() ? \file_get_contents($source) : $source,
+            ]),
+            $this->environment->getLoader(),
+        ]));
+
+        return $this->environment->render($template, $parameters);
+    }
+
+    /**
+     * @param string $charset
+     */
+    public function setCharset(string $charset): void
+    {
+        $this->environment->setCharset($charset);
+    }
+
+    /**
+     * Sets the current cache implementation.
+     *
+     * @param CacheInterface|false|string $cache A Twig\Cache\CacheInterface implementation,
+     *                                           an absolute path to the compiled templates,
+     *                                           or false to disable cache
+     */
+    public function setCache($cache): void
+    {
+        $this->environment->setCache($cache);
+    }
+
+    /**
+     * @param RuntimeLoaderInterface $loader
+     */
+    public function addRuntimeLoader(RuntimeLoaderInterface $loader): void
+    {
+        $this->environment->addRuntimeLoader($loader);
+    }
+
+    /**
+     * @param ExtensionInterface $extension
+     */
+    public function addExtension(ExtensionInterface $extension): void
+    {
+        $this->environment->addExtension($extension);
+    }
+
+    /**
+     * @param ExtensionInterface[] $extensions An array of extensions
+     */
+    public function setExtensions(array $extensions): void
+    {
+        $this->environment->setExtensions($extensions);
+    }
+
+    /**
+     * @param NodeVisitorInterface $visitor
+     */
+    public function addNodeVisitor(NodeVisitorInterface $visitor): void
+    {
+        $this->environment->addNodeVisitor($visitor);
+    }
+
+    /**
+     * @param TwigFilter $filter
+     */
+    public function addFilter(TwigFilter $filter): void
+    {
+        $this->environment->addFilter($filter);
+    }
+
+    /**
+     * @param callable $callable
+     */
+    public function registerUndefinedFilterCallback(callable $callable): void
+    {
+        $this->environment->registerUndefinedFilterCallback($callable);
+    }
+
+    /**
+     * @param TwigFunction $function
+     */
+    public function addFunction(TwigFunction $function): void
+    {
+        $this->environment->addFunction($function);
+    }
+}
