22fb66e335d0d418f1091e636d787740f10af174
Divine Niiquaye Ibok <divineibok@gmail.com>
2020-10-23 00:58:35 +0000

    Updated cache storage for templating

--- packages/templating/src/Storage/CacheStorage.php
+++ packages/templating/src/Storage/CacheStorage.php
@@ -39,18 +39,18 @@ class CacheStorage implements StorageInterface
     protected $logger;
 
     /** @var string */
-    protected $dir;
+    protected $directory;
 
     /**
      * @param StorageInterface     $storage
-     * @param string               $dir     The directory where to store the cache files
+     * @param string               $directory The directory where to store the cache files
      * @param null|LoggerInterface $logger
      */
-    public function __construct(StorageInterface $storage, string $dir, ?LoggerInterface $logger = null)
+    public function __construct(StorageInterface $storage, string $directory, ?LoggerInterface $logger = null)
     {
-        $this->logger  = $logger;
-        $this->storage = $storage;
-        $this->dir     = $dir;
+        $this->logger    = $logger;
+        $this->storage   = $storage;
+        $this->directory = \rtrim($directory, '\/') . '/';
     }
 
     /**
@@ -67,11 +67,11 @@ class CacheStorage implements StorageInterface
     public function load(string $template): ?string
     {
         $key  = \hash('sha256', $template);
-        $dir  = $this->dir . \DIRECTORY_SEPARATOR . \substr($key, 0, 2);
+        $dir  = $this->directory . \substr($key, 0, 2);
         $file = \substr($key, 2) . '.ui.phtml';
         $path = $dir . \DIRECTORY_SEPARATOR . $file;
 
-        if (\is_file($path)) {
+        if (\is_file($path) && !$this->isFresh($template, @filemtime($path))) {
             if (null !== $this->logger) {
                 $this->logger->debug('Fetching template from cache.', ['name' => $template]);
             }
@@ -83,18 +83,69 @@ class CacheStorage implements StorageInterface
             return null;
         }
 
-        $content = file_exists($storage) ? \file_get_contents($storage) : $storage;
-
-        if (!\is_dir($dir) && !@\mkdir($dir, 0777, true) && !\is_dir($dir)) {
-            throw new RuntimeException(\sprintf('Cache Loader was not able to create directory "%s".', $dir));
-        }
-
-        \file_put_contents($path, $content);
+        $this->write($path, \file_exists($storage) ? \file_get_contents($storage) : $storage);
 
         if (null !== $this->logger) {
             $this->logger->debug('Storing template in cache.', ['name' => $template]);
         }
 
         return \file_get_contents($path);
+    }
+
+    /**
+     * @param string $key
+     * @param string $content
+     */
+    private function write(string $key, string $content): void
+    {
+        $dir = \dirname($key);
+
+        if (!\is_dir($dir)) {
+            if (false === @\mkdir($dir, 0777, true)) {
+                \clearstatcache(true, $dir);
+
+                if (!\is_dir($dir)) {
+                    throw new RuntimeException(\sprintf('Unable to create the cache directory (%s).', $dir));
+                }
+            }
+        } elseif (!\is_writable($dir)) {
+            throw new RuntimeException(\sprintf('Unable to write in the cache directory (%s).', $dir));
+        }
+
+        $tmpFile = \tempnam($dir, \basename($key));
+
+        if (false !== @\file_put_contents($tmpFile, $content) && @\rename($tmpFile, $key)) {
+            @\chmod($key, 0666 & ~\umask());
+
+            // Compile cached file into bytecode cache
+            if (\function_exists('opcache_invalidate') && \filter_var(\ini_get('opcache.enable'), \FILTER_VALIDATE_BOOLEAN)) {
+                @\opcache_invalidate($key, true);
+            } elseif (\function_exists('apc_compile_file')) {
+                apc_compile_file($key);
+            }
+
+            return;
+        }
+
+        throw new RuntimeException(\sprintf('Failed to write cache file "%s".', $key));
+    }
+
+    /**
+     * Returns true if the template is still fresh.
+     *
+     * Besides checking the loader for freshness information,
+     * this method also checks if the enabled extensions have
+     * not changed.
+     *
+     * @param string $name
+     * @param int $time The last modification time of the cached template
+     */
+    private function isFresh(string $name, int $time): bool
+    {
+        if (null === $path = $this->storage->load($name)) {
+            return false;
+        }
+
+        return \filemtime($path) < $time;
     }
 }
