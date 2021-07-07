e3640e311527be36d3bd46f704a7b5f08b039410
Divine Niiquaye Ibok <divineibok@gmail.com>
2021-07-07 23:51:02 +0000

    Updated PSR-7 factory for laminas

--- packages/http-galaxy/src/Factories/LaminasPsr7Factory.php
+++ packages/http-galaxy/src/Factories/LaminasPsr7Factory.php
@@ -17,156 +17,121 @@ declare(strict_types=1);
 
 namespace Biurad\Http\Factories;
 
+use Biurad\Http\Interfaces\Psr17Interface;
+use Biurad\Http\Request;
+use Biurad\Http\Response;
+use Biurad\Http\ServerRequest;
+use Biurad\Http\Stream;
+use Biurad\Http\UploadedFile;
+use Biurad\Http\Uri;
 use Laminas\Diactoros\RequestFactory;
 use Laminas\Diactoros\ResponseFactory;
-use Laminas\Diactoros\ServerRequest;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\Diactoros\StreamFactory;
 use Laminas\Diactoros\UploadedFileFactory;
 use Laminas\Diactoros\UriFactory;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-
-use function Laminas\Diactoros\marshalHeadersFromSapi;
-use function Laminas\Diactoros\marshalMethodFromSapi;
-use function Laminas\Diactoros\marshalProtocolVersionFromSapi;
-use function Laminas\Diactoros\marshalUriFromSapi;
-use function Laminas\Diactoros\normalizeServer;
-use function Laminas\Diactoros\normalizeUploadedFiles;
-use function Laminas\Diactoros\parseCookieHeader;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\UriInterface;
 
 /**
  * @author Divine Niiquaye Ibok <divineibok@gmail.com>
  */
-class LaminasPsr7Factory extends Psr17Factory
+class LaminasPsr7Factory implements Psr17Interface
 {
     /**
-     * Function to use to get apache request headers; present only to simplify mocking.
-     *
-     * @var callable
-     */
-    private static $apacheRequestHeaders = 'apache_request_headers';
-
-    /**
      * {@inheritdoc}
      */
-    public static function fromGlobalRequest(
-        array $server = null,
-        array $query = null,
-        array $body = null,
-        array $cookies = null,
-        array $files = null
-    ): ServerRequestInterface {
-        $server = normalizeServer(
-            $server ?: $_SERVER,
-            \is_callable(self::$apacheRequestHeaders) ? self::$apacheRequestHeaders : 'getallheaders'
-        );
-        $files   = normalizeUploadedFiles($files ?: $_FILES);
-        $headers = self::getHeaders(marshalHeadersFromSapi($server));
-
-        if (null === $cookies && \array_key_exists('cookie', $headers)) {
-            $cookies = parseCookieHeader($headers['cookie']);
-        }
-
-        return new ServerRequest(
-            $server,
-            $files,
-            marshalUriFromSapi($server, $headers),
-            marshalMethodFromSapi($server),
-            'php://input',
-            $headers,
-            $cookies ?: $_COOKIE,
-            $query ?: $_GET,
-            $body ?: $_POST,
-            marshalProtocolVersionFromSapi($server)
-        );
+    public function createRequest(string $method, $uri): RequestInterface
+    {
+        return (new Request($method, $uri))->withRequest((new RequestFactory())->createRequest($method, $uri));
     }
 
     /**
      * {@inheritdoc}
      */
-    protected function setFactoryCandidates(): void
+    public function createResponse(int $code = 200, string $reasonPhrase = 'Ok'): ResponseInterface
     {
-        $this->responseFactoryClass         = ResponseFactory::class;
-        $this->serverRequestFactoryClass    = ServerRequestFactory::class;
-        $this->requestFactoryClass          = RequestFactory::class;
-        $this->uploadedFilesFactoryClass    = UploadedFileFactory::class;
-        $this->streamFactoryClass           = StreamFactory::class;
-        $this->uriFactoryClass              = UriFactory::class;
+        return (new Response())->withResponse((new ResponseFactory())->createResponse($code, $reasonPhrase));
     }
 
     /**
-     * Gets the HTTP headers.
-     *
-     * @return array
+     * {@inheritdoc}
      */
-    private static function getHeaders(array $parameters): array
+    public function createServerRequest(string $method, $uri, array $serverParams = []): ServerRequestInterface
     {
-        $headers = [];
-
-        foreach ($parameters as $key => $value) {
-            $headers[$key] = $value;
-        }
-
-        if (isset($parameters['PHP_AUTH_USER'])) {
-            $headers['PHP_AUTH_USER'] = $parameters['PHP_AUTH_USER'];
-            $headers['PHP_AUTH_PW']   = $parameters['PHP_AUTH_PW'] ?? '';
-        } else {
-            /*
-             * php-cgi under Apache does not pass HTTP Basic user/pass to PHP by default
-             * For this workaround to work, add these lines to your .htaccess file:
-             * RewriteCond %{HTTP:Authorization} .+
-             * RewriteRule ^ - [E=HTTP_AUTHORIZATION:%0]
-             *
-             * A sample .htaccess file:
-             * RewriteEngine On
-             * RewriteCond %{HTTP:Authorization} .+
-             * RewriteRule ^ - [E=HTTP_AUTHORIZATION:%0]
-             * RewriteCond %{REQUEST_FILENAME} !-f
-             * RewriteRule ^(.*)$ app.php [QSA,L]
-             */
-
-            $authorizationHeader = null;
-
-            if (isset($parameters['AUTHORIZATION'])) {
-                $authorizationHeader = $parameters['AUTHORIZATION'];
-            }
-
-            if (null !== $authorizationHeader) {
-                if (0 === \stripos($authorizationHeader, 'basic ')) {
-                    // Decode AUTHORIZATION header into PHP_AUTH_USER and PHP_AUTH_PW when authorization header is basic
-                    $exploded = \explode(':', \base64_decode(\substr($authorizationHeader, 6)), 2);
-
-                    if (2 == \count($exploded)) {
-                        [$headers['PHP_AUTH_USER'], $headers['PHP_AUTH_PW']] = $exploded;
-                    }
-                } elseif (empty($parameters['PHP_AUTH_DIGEST']) && (0 === \stripos($authorizationHeader, 'digest '))) {
-                    // In some circumstances PHP_AUTH_DIGEST needs to be set
-                    $headers['PHP_AUTH_DIGEST']    = $authorizationHeader;
-                    $parameters['PHP_AUTH_DIGEST'] = $authorizationHeader;
-                } elseif (0 === \stripos($authorizationHeader, 'bearer ')) {
-                    /*
-                     * XXX: Since there is no PHP_AUTH_BEARER in PHP predefined variables,
-                     *      I'll just set $headers['AUTHORIZATION'] here.
-                     *      https://php.net/reserved.variables.server
-                     */
-                    $headers['AUTHORIZATION'] = $authorizationHeader;
-                }
+        if (empty($method)) {
+            if (!empty($serverParams['REQUEST_METHOD'])) {
+                $method = $serverParams['REQUEST_METHOD'];
+            } else {
+                throw new \InvalidArgumentException('Cannot determine HTTP method');
             }
         }
 
-        if (isset($headers['AUTHORIZATION'])) {
-            return $headers;
+        return (new ServerRequest($method, $uri, [], null, '1.1', $serverParams))
+            ->withRequest((new ServerRequestFactory())->createServerRequest($method, $uri, $serverParams));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createStream(string $content = ''): StreamInterface
+    {
+        return (new Stream())->withStream((new StreamFactory)->createStream($content));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createStreamFromFile(string $file, string $mode = 'r'): StreamInterface
+    {
+        return (new Stream())->withStream((new StreamFactory)->createStreamFromFile($file, $mode));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createStreamFromResource($resource): StreamInterface
+    {
+        return (new Stream())->withStream((new StreamFactory)->createStreamFromResource($resource));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createUploadedFile(
+        StreamInterface $stream,
+        ?int $size = null,
+        int $error = \UPLOAD_ERR_OK,
+        ?string $clientFilename = null,
+        ?string $clientMediaType = null
+    ): UploadedFileInterface {
+        if (null === $size) {
+            $size = $stream->getSize();
         }
 
-        // PHP_AUTH_USER/PHP_AUTH_PW
-        if (isset($headers['PHP_AUTH_USER'])) {
-            $headers['AUTHORIZATION'] = 'Basic '
-                . \base64_encode($headers['PHP_AUTH_USER'] . ':'
-                . $headers['PHP_AUTH_PW']);
-        } elseif (isset($headers['PHP_AUTH_DIGEST'])) {
-            $headers['AUTHORIZATION'] = $headers['PHP_AUTH_DIGEST'];
-        }
+        return (new UploadedFile('', $size, $error, $clientFilename, $clientMediaType))
+            ->withUploadFile((new UploadedFileFactory())->createUploadedFile($stream, $size, $error, $clientFilename, $clientMediaType));
+    }
 
-        return $headers;
+    /**
+     * {@inheritdoc}
+     */
+    public function createUri(string $uri = ''): UriInterface
+    {
+        return (new Uri($uri))->withUri((new UriFactory())->createUri($uri));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function fromGlobalRequest(): ServerRequestInterface
+    {
+        $method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+
+        return (new ServerRequest($method, ''))->withRequest(ServerRequestFactory::fromGlobals());
     }
 }
