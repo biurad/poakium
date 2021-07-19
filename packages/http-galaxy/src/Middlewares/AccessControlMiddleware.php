a19e7442f3b14aad9aed8c2a77740f26a198d845
Divine Niiquaye Ibok <divineibok@gmail.com>
2021-07-19 17:11:29 +0000

    Updated http cors middleware
    - Fixed Access-Control- prefixed headers.
    - Updated config from array object class to array itself.
    - Fixed major issues with access headers.

--- packages/http-galaxy/src/Middlewares/AccessControlMiddleware.php
+++ packages/http-galaxy/src/Middlewares/AccessControlMiddleware.php
@@ -17,6 +17,7 @@ declare(strict_types=1);
 
 namespace Biurad\Http\Middlewares;
 
+use Biurad\Http\Interfaces\RequestMatcherInterface;
 use Biurad\Http\Strategies\RequestMatcher;
 use Fig\Http\Message\StatusCodeInterface;
 use Psr\Http\Message\RequestInterface;
@@ -38,22 +39,27 @@ use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
  */
 class AccessControlMiddleware implements MiddlewareInterface
 {
-    /** @var \ArrayObject */
+    /** @var RequestMatcher */
+    protected $matcher;
+
+    /** @var array<string,mixed> */
     protected $options;
 
     /**
-     * Simple headers as defined in the spec should always be accepted
+     * Simple headers as defined in the spec should always be accepted.
      */
     protected static $simpleHeaders = [
         'accept',
         'accept-language',
         'content-language',
         'origin',
+        'expires',
     ];
 
-    public function __construct(array $options = [])
+    public function __construct(array $options = [], RequestMatcherInterface $requestMatcher = null)
     {
-        $this->options = new \ArrayObject($this->normalizeOptions($options));
+        $this->matcher = $requestMatcher ?? new RequestMatcher();
+        $this->options = $this->normalizeOptions($options);
     }
 
     /**
@@ -69,8 +75,8 @@ class AccessControlMiddleware implements MiddlewareInterface
         }
 
         // skip if not a CORS request
-        if (!$request->hasHeader('Origin') || $this->isSameHost($request)) {
-            return $response;
+        if (!$this->hasOrigin($request)) {
+            return $response->withoutHeader('Access-Control-Allow-Origin');
         }
 
         // perform preflight checks
@@ -82,146 +88,138 @@ class AccessControlMiddleware implements MiddlewareInterface
     }
 
     /**
-     * For Preflight, return the Preflight response
-     *
-     * @param RequestInterface  $request
-     * @param ResponseInterface $response
-     *
-     * @return ResponseInterface
+     * For Preflight, return the Preflight response.
      */
     protected function getPreflightResponse(RequestInterface $request, ResponseInterface $response): ResponseInterface
     {
-        $options = $this->options;
+        $response = $response
+            ->withHeader('Access-Control-Allow-Origin', true === $this->options['allow_origin'] ? '*' : $request->getHeaderLine('Origin'))
+            ->withHeader('Vary', $response->hasHeader('Vary') ? $response->getHeaderLine('Vary') . ', Origin' : 'Origin');
 
-        if (!$response->hasHeader('Vary')) {
-            $response = $response->withHeader('Vary', 'Origin');
-        } else {
-            $response = $response->withHeader('Vary', $response->getHeaderLine('Vary') . ', Origin');
-        }
-
-        if ($options['allow_credentials']) {
+        if ($this->options['allow_credentials']) {
             $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
         }
 
-        if (!empty($options['allow_methods'])) {
-            $response = $response->withHeader('Access-Control-Allow-Methods', \implode(', ', $options['allow_methods']));
-        }
+        // check request headers
+        if (isset($this->options['allow_headers'])) {
+            $allowedHeaders = $this->options['allow_headers'];
 
-        if ($options['allow_headers']) {
-            $headers = $this->isWildcard('allow_headers')
-                ? $request->getHeaderLine('Access-Control-Request-Headers')
-                : \implode(', ', $options['allow_headers']);
+            if ($request->hasHeader('Access-Control-Request-Headers')) {
+                $headers = \trim($request->getHeaderLine('Access-Control-Request-Headers'));
 
-            if ($headers) {
-                $response = $response->withHeader('Access-Control-Allow-Headers', $headers);
+                foreach (\preg_split('{, *}', $headers) as $header) {
+                    if (\in_array(\strtolower($header), self::$simpleHeaders, true)) {
+                        continue;
+                    }
+
+                    if (\is_array($allowedHeaders) && !\in_array($header, $allowedHeaders, true)) {
+                        return $response->withStatus(StatusCodeInterface::STATUS_BAD_REQUEST);
+                    }
+                }
+            }
+
+            if (isset($headers) || (\is_array($allowedHeaders) && !empty($allowedHeaders))) {
+                $response = $response->withHeader('Access-Control-Allow-Headers', $headers ?? \implode(', ', $allowedHeaders));
             }
         }
 
-        if ($options['max_age']) {
-            $response = $response->withHeader('Access-Control-Max-Age', $options['max_age']);
-        }
+        if (isset($this->options['expose_headers'])) {
+            $exposedHeaders = [];
 
-        if (!$this->checkOrigin($request)) {
-            return $response->withoutHeader('Access-Control-Allow-Origin');
-        }
-
-        $response = $response->withHeader('Access-Control-Allow-Origin', $request->getHeaderLine('Origin'));
-
-        // check request method
-        if (!\in_array(\strtoupper($request->getHeaderLine('Access-Control-Request-Method')), $options['allow_methods'], true)) {
-            return $response->withStatus(StatusCodeInterface::STATUS_METHOD_NOT_ALLOWED);
-        }
-
-        /*
-         * We have to allow the header in the case-set as we received it by the client.
-         * Firefox f.e. sends the LINK method as "Link", and we have to allow it like this or the browser will deny the
-         * request.
-         */
-        if (!\in_array($request->getHeaderLine('Access-Control-Request-Method'), $options['allow_methods'], true)) {
-            $options['allow_methods'][] = $request->getHeaderLine('Access-Control-Request-Method');
-
-            $response = $response->withHeader('Access-Control-Allow-Methods', \implode(', ', $options['allow_methods']));
-        }
-
-        // check request headers
-        $headers = $request->getHeaderLine('Access-Control-Request-Headers');
-
-        if ($headers && !$this->isWildcard('allow_headers')) {
-            $headers = \strtolower(\trim($headers));
-
-            foreach (\preg_split('{, *}', $headers) as $header) {
+            foreach ((array) $this->options['expose_headers'] as $header) {
                 if (\in_array(\strtolower($header), self::$simpleHeaders, true)) {
                     continue;
                 }
 
-                if (!\in_array($header, $options['allow_headers'], true)) {
-                    $response = $response->withStatus(StatusCodeInterface::STATUS_BAD_REQUEST);
-
-                    break;
-                }
+                $exposedHeaders[] = $headers;
             }
+
+            if (!empty($exposedHeaders)) {
+                $response = $response->withHeader('Access-Control-Expose-Headers', \implode(', ', $exposedHeaders));
+            }
+        }
+
+        if ($this->options['max_age'] > 0) {
+            $response = $response->withHeader('Access-Control-Max-Age', (string) $this->options['max_age']);
+        }
+
+        if (!empty($allowedMethods = $this->options['allow_methods'])) {
+            $requestMethod = \strtoupper($request->getHeaderLine('Access-Control-Request-Method'));
+
+            /*
+             * We have to allow the header in the case-set as we received it by the client.
+             * Firefox f.e. sends the LINK method as "Link", and we have to allow it like this or the browser will deny the request.
+             */
+            if (isset($allowedMethods['Link'])) {
+                $allowedMethods[] = 'link';
+            }
+
+            // check request method
+            if (!\in_array($requestMethod, $allowedMethods, true)) {
+                return $response->withStatus(StatusCodeInterface::STATUS_METHOD_NOT_ALLOWED);
+            }
+
+            $response = $response->withHeader('Access-Control-Allow-Methods', \implode(', ', $allowedMethods));
         }
 
         return $response;
     }
 
     /**
-     * Checks the Origin.
-     *
-     * @param RequestInterface $request
-     *
-     * @return bool
+     * Checks the request Origin and whether or not has same host.
      */
-    protected function checkOrigin(RequestInterface $request): bool
+    protected function hasOrigin(RequestInterface $request): bool
     {
-        // check origin
-        $origin = $request->getHeaderLine('Origin');
-
-        if ($this->isWildcard('allow_origin')) {
+        if (true === $allowedOrigin = $this->options['allow_origin']) {
             return true;
         }
 
-        if ($this->options['origin_regex'] === true) {
+        if (empty($requestOrigin = $request->getHeaderLine('Origin'))) {
+            goto cors_same_host;
+        }
+
+        if (\is_array($allowedOrigin) && !empty($allowedOrigin)) {
+            $allowedOrigin = (array) $allowedOrigin;
+
             // origin regex matching
-            foreach ((array) $this->options['allow_origin'] as $originRegexp) {
-                if (\preg_match('{' . $originRegexp . '}i', $origin)) {
-                    return true;
+            if (true === $this->options['origin_regex']) {
+                foreach ($allowedOrigin as $originRegexp) {
+                    if (!\is_string($originRegexp)) {
+                        continue;
+                    }
+
+                    if (\preg_match('{' . $originRegexp . '}i', $requestOrigin)) {
+                        return true;
+                    }
                 }
-            }
-        } else {
-            // old origin matching
-            if (\in_array($origin, $this->options['allow_origin'])) {
-                return true;
+            } elseif (\in_array($requestOrigin, $allowedOrigin, true)) {
+                return true; // old origin matching
             }
         }
 
-        return false;
+        cors_same_host: // Checks if Origin header value is same as domain.
+        $requestHost = ($requestUri = $request->getUri())->getScheme() . '://' . $requestUri->getHost();
+
+        return !$requestOrigin === $requestHost || !$requestOrigin === (string) $requestUri;
     }
 
     /**
-     * The the path from the config, to see if the CORS Service should run
-     *
-     * @param RequestInterface $request
-     *
-     * @return bool
+     * The the path from the config, to see if the CORS Service should run.
      */
     private function isMatchingPath(RequestInterface $request): bool
     {
-        $paths   = $this->options['allow_paths'];
-        $matcher = new RequestMatcher();
-
-        if (is_bool($paths) && $this->isWildcard('allow_paths')) {
+        if (true === $paths = $this->options['allow_paths']) {
             return true;
         }
 
         foreach ($paths as $pathRegexp => $options) {
-            $matcher->matchPath($pathRegexp);
+            $this->matcher->matchPath($pathRegexp);
 
             // skip if the host is not matching
-            $matcher->matchHost($options['hosts'] ?? []);
+            $this->matcher->matchHost($options['hosts'] ?? []);
 
-            if ($matcher->matches($request)) {
-                $this->options->exchangeArray(array_merge($this->options->getArrayCopy(), $options));
+            if ($this->matcher->matches($request)) {
+                $this->options = \array_merge($this->options, $options);
 
                 return true;
             }
@@ -231,58 +229,25 @@ class AccessControlMiddleware implements MiddlewareInterface
     }
 
     /**
-     * Is the option a wildcard type
+     * Normalize the options for usage.
      *
-     * @param string $option
+     * @param array<string,mixed> $options
      *
-     * @return bool
-     */
-    private function isWildcard(string $option): bool
-    {
-        return $this->options[$option] === true || (\is_array($this->options[$option]) && \in_array('*', $this->options[$option]));
-    }
-
-    /**
-     * Checks if Origin header value is same as domain.
-     *
-     * @param ServerRequestInterface $request
-     *
-     * @return bool
-     */
-    private function isSameHost(ServerRequestInterface $request): bool
-    {
-        $uri = $request->getUri()->getScheme() . '://' . $request->getUri()->getHost();
-
-        return $request->getHeaderLine('Origin') === $uri;
-    }
-
-    /**
-     * Normalize the options for usage
-     *
-     * @param array $options
-     *
-     * @return array
+     * @return array<string,mixed>
      */
     private function normalizeOptions(array $options = []): array
     {
-        $options = \array_merge([
-            'allow_origin'      => [],
-            'allow_paths'       => [],
-            'origin_regex'      => false,
+        $defaultOptions = [
+            'allow_origin' => [],
+            'allow_paths' => [],
+            'origin_regex' => false,
             'allow_credentials' => false,
-            'allow_headers'     => [],
-            'expose_headers'    => [],
-            'allow_methods'     => [],
-            'max_age'           => 0,
-        ], $options);
+            'allow_headers' => null,
+            'expose_headers' => null,
+            'allow_methods' => [],
+            'max_age' => 0,
+        ];
 
-        // normalize array('*') to true
-        foreach (['allow_origin', 'allow_headers', 'allow_paths'] as $wildcard) {
-            if ($options[$wildcard] === '*') {
-                $options[$wildcard] = true;
-            }
-        }
-
-        return $options;
+        return $options += $defaultOptions;
     }
 }
