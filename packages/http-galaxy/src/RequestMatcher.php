7c2087b46987b716f537965b5912cbf1fc2221ae
Divine Niiquaye Ibok <divineibok@gmail.com>
2021-07-26 00:48:19 +0000

    Moved request matcher class under main namespace

--- packages/http-galaxy/src/RequestMatcher.php
+++ packages/http-galaxy/src/RequestMatcher.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Biurad\Http\Strategies;
+namespace Biurad\Http;
 
 use Biurad\Http\Interfaces\RequestMatcherInterface;
 use Biurad\Http\ServerRequest;
