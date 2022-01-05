3f421477ecea9c9f25a743742fad44c7b7d2b5ed
Divine Niiquaye Ibok <divineibok@gmail.com>
2022-01-05 13:57:19 +0000

    Updated session middleware class

--- packages/http-galaxy/src/Middlewares/SessionMiddleware.php
+++ packages/http-galaxy/src/Middlewares/SessionMiddleware.php
@@ -17,27 +17,38 @@ declare(strict_types=1);
 
 namespace Biurad\Http\Middlewares;
 
-use Biurad\Http\Interfaces\SessionInterface;
-use Biurad\Http\Session;
+use Biurad\Http\Request as HttpRequest;
+use Biurad\Http\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionUtils;
 
 class SessionMiddleware implements MiddlewareInterface
 {
     // request attribute
     public const ATTRIBUTE = Session::class;
 
-    /** @var SessionInterface */
+    /** @var callable(): SessionInterface */
     protected $session;
 
     /**
-     * Create a new session middleware.
+     * @var array<string, mixed>
      */
-    public function __construct(SessionInterface $session)
+    private $sessionOptions;
+
+    /**
+     * Create a new session middleware.
+     *
+     * @param array<string,mixed> $sessionOptions
+     */
+    public function __construct(callable $sessionCallback, array $sessionOptions = [])
     {
-        $this->session = $session;
+        $this->session = $sessionCallback;
+        $this->sessionOptions = $sessionOptions;
     }
 
     /**
@@ -45,11 +56,36 @@ class SessionMiddleware implements MiddlewareInterface
      */
     public function process(Request $request, RequestHandler $handler): ResponseInterface
     {
-        $this->session->setRequestOnHandler($request); // For cookie handlers
+        if ($request instanceof HttpRequest) {
+            if (!$request->getRequest()->hasSession(true)) {
+                // This variable prevents calling `$this->getSession()` twice in case the Request (and the below factory) is cloned
+                $sess = null;
+                $request->getRequest()->setSessionFactory(function () use (&$sess, $request) {
+                    if (!$sess) {
+                        $sess = ($this->session)();
+                    }
 
-        $response = $handler->handle($request->withAttribute(static::ATTRIBUTE, $this->session));
+                    /*
+                    * For supporting sessions in php runtime with runners like roadrunner or swoole the session
+                    * cookie need read from the cookie bag and set on the session storage.
+                    */
+                    if ($sess && !$sess->isStarted()) {
+                        $sessionId = $request->getCookieParams()[$sess->getName()] ?? '';
+                        $sess->setId($sessionId);
+                    }
 
-        if ($this->session->isStarted()) {
+                    return $sess;
+                });
+            }
+
+            $session = $request->getRequest()->getSession();
+        } elseif (null === $session = $request->getAttribute(Session::class)) {
+            $request = $request->withAttribute(static::ATTRIBUTE, $session = ($this->session)());
+        }
+
+        $response = $handler->handle($request);
+
+        if ($session->isStarted()) {
             /*
              * Saves the session, in case it is still open, before sending the response/headers.
              *
@@ -74,9 +110,89 @@ class SessionMiddleware implements MiddlewareInterface
              * Biurad session implementation starts the session on demand. So writing to it after
              * it is saved will just restart it.
              */
-            $this->session->save();
+            $session->save();
+
+            /*
+             * For supporting sessions in php runtime with runners like roadrunner or swoole the session
+             * cookie need to be written on the response object and should not be written by PHP itself.
+             */
+            $sessionName = $session->getName();
+            $sessionId = $session->getId();
+            $sessionOptions = $this->getSessionOptions($this->sessionOptions);
+            $sessionCookiePath = $sessionOptions['cookie_path'] ?? '/';
+            $sessionCookieDomain = $sessionOptions['cookie_domain'] ?? null;
+            $sessionCookieSecure = $sessionOptions['cookie_secure'] ?? false;
+            $sessionCookieHttpOnly = $sessionOptions['cookie_httponly'] ?? true;
+            $sessionCookieSameSite = $sessionOptions['cookie_samesite'] ?? Cookie::SAMESITE_LAX;
+
+            SessionUtils::popSessionCookie($sessionName, $sessionId);
+            $requestSessionCookieId = $request->getCookieParams()[$sessionName] ?? null;
+
+            if ($requestSessionCookieId && ($session instanceof Session ? $session->isEmpty() : empty($session->all()))) {
+                $cookie = new Cookie(
+                    $sessionName,
+                    null,
+                    1,
+                    $requestSessionCookieId,
+                    $sessionCookiePath,
+                    $sessionCookieDomain,
+                    $sessionCookieSecure,
+                    $sessionCookieHttpOnly,
+                    $sessionCookieSameSite
+                );
+
+                if ($response instanceof Response) {
+                    $response = $response->withCookie($cookie);
+                } else {
+                    $response = $response->withAddedHeader('Set-Cookie', (string) $cookie);
+                }
+            } elseif ($sessionId !== $requestSessionCookieId) {
+                $expire = 0;
+                $lifetime = $sessionOptions['cookie_lifetime'] ?? null;
+
+                if ($lifetime) {
+                    $expire = \time() + $lifetime;
+                }
+
+                $cookie = new Cookie(
+                    $sessionName,
+                    $sessionId,
+                    $expire,
+                    $sessionCookiePath,
+                    $sessionCookieDomain,
+                    $sessionCookieSecure,
+                    $sessionCookieHttpOnly,
+                    false,
+                    $sessionCookieSameSite
+                );
+
+                if ($response instanceof Response) {
+                    $response = $response->withCookie($cookie);
+                } else {
+                    $response = $response->withAddedHeader('Set-Cookie', (string) $cookie);
+                }
+            }
         }
 
         return $response;
+    }
+
+    private function getSessionOptions(array $sessionOptions): array
+    {
+        $mergedSessionOptions = [];
+
+        foreach (\session_get_cookie_params() as $key => $value) {
+            $mergedSessionOptions['cookie_' . $key] = $value;
+        }
+
+        foreach ($sessionOptions as $key => $value) {
+            // do the same logic as in the NativeSessionStorage
+            if ('cookie_secure' === $key && 'auto' === $value) {
+                continue;
+            }
+            $mergedSessionOptions[$key] = $value;
+        }
+
+        return $mergedSessionOptions;
     }
 }
