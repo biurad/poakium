53a0c1b58bf8d437501f23bfe6e5d727bce90b6b
Divine Niiquaye Ibok <divineibok@gmail.com>
2022-09-01 10:25:55 +0000

    Added php-cs standard rules & fixer support

---
+++ .php_cs.dist.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Biurad opensource projects.
+ *
+ * @copyright 2022 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$header = <<<'EOF'
+This file is part of Biurad opensource projects.
+
+@copyright 2022 Biurad Group (https://biurad.com/)
+@license   https://opensource.org/licenses/BSD-3-Clause License
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+EOF;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setFinder(
+        (new PhpCsFixer\Finder())
+            ->in(__DIR__)
+            ->append([__FILE__])
+            ->notPath('#/Fixtures/#')
+            ->exclude([
+                'vendor/',
+                'packages/*/vendor/',
+            ])
+    )
+    ->setRules([
+        '@PhpCsFixer' => true,
+        '@Symfony:risky' => true,
+        '@PHP80Migration:risky' => true,
+        '@PHPUnit84Migration:risky' => true,
+        'protected_to_private' => false,
+        'single_import_per_statement' => false,
+        'blank_line_after_opening_tag' => false,
+        'linebreak_after_opening_tag' => false,
+        'single_class_element_per_statement' => false,
+        'native_constant_invocation' => true,
+        'comment_to_phpdoc' => true,
+        'strict_param' => true,
+        'no_unset_on_property' => true,
+        'nullable_type_declaration_for_default_null_value' => ['use_nullable_type_declaration' => false],
+        'header_comment' => ['header' => $header],
+        'native_function_invocation' => [
+            'include' => ['@compiler_optimized', '@internal'],
+            'strict' => true,
+        ],
+        'general_phpdoc_tag_rename' => [
+            'replacements' => [
+                'inheritDocs' => 'inheritdoc',
+                'inheritDoc' => 'inheritdoc',
+            ],
+        ],
+        'blank_line_before_statement' => [
+            'statements' => [],
+        ],
+    ])
+;
