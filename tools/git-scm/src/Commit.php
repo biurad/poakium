d72efd2533184eab3f6ab84bf783e4399e2ad58b
Divine Niiquaye Ibok <divineibok@gmail.com>
2022-09-04 09:09:29 +0000

    Added a commit class to git-scm

---
+++ tools/git-scm/src/Commit.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Biurad opensource projects.
+ *
+ * @copyright 2022 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Biurad\Git;
+
+/**
+ * Represents a git commit.
+ *
+ * @author Divine Niiquaye Ibok <divineibok@gmail.com>
+ */
+class Commit extends GitObject
+{
+}
