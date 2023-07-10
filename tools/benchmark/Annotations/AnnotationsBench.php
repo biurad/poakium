d0d7efcb9636fc1963a2ebab9b250270b74e1128
Divine Niiquaye Ibok <divineibok@gmail.com>
2023-07-10 08:09:56 +0000

    Added benchmark for annotations

---
+++ tools/benchmark/Annotations/AnnotationsBench.php
@@ -0,0 +1,143 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Biurad opensource projects.
+ *
+ * @copyright 2022 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Biurad\Benchmark\Annotations;
+
+use Biurad\Annotations\AnnotationLoader;
+use Biurad\Annotations\Tests\Fixtures\SampleListener;
+use Doctrine\Common\Annotations\AnnotationReader as DoctrineAnnotationReader;
+use Spiral\Attributes\AnnotationReader;
+use Spiral\Attributes\AttributeReader;
+use Spiral\Attributes\Composite\MergeReader;
+use Spiral\Attributes\Composite\SelectiveReader;
+use Spiral\Attributes\Internal\FallbackAttributeReader;
+
+/**
+ * @Iterations(5)
+ *
+ * @Revs(500)
+ */
+class AnnotationsBench
+{
+    protected const DIRECTORIES = [
+        __DIR__.'/../../../packages/annotations/tests/Fixtures/Annotation/Valid',
+        __DIR__.'/../../../packages/annotations/tests/Fixtures/Annotation/Attribute',
+        'Biurad\Annotations\Tests\Fixtures\Annotation\annotated_function',
+        'SampleFunc',
+    ];
+
+    /** @var AnnotationLoader */
+    private $annotation;
+
+    public function __construct()
+    {
+        DoctrineAnnotationReader::addGlobalIgnoredName('BeforeMethods');
+        DoctrineAnnotationReader::addGlobalIgnoredName('Iterations');
+        DoctrineAnnotationReader::addGlobalIgnoredName('Revs');
+    }
+
+    public function createSelectiveLoaderInstance(): void
+    {
+        $annotation = new AnnotationLoader(new SelectiveReader([new AttributeReader(), new AnnotationReader()]));
+
+        $annotation->listener(new SampleListener());
+        $annotation->resource(...self::DIRECTORIES);
+
+        $this->annotation = $annotation;
+    }
+
+    public function createMergedLoaderInstance(): void
+    {
+        $annotation = new AnnotationLoader(new MergeReader([new AttributeReader(), new AnnotationReader()]));
+
+        $annotation->listener(new SampleListener());
+        $annotation->resource(...self::DIRECTORIES);
+
+        $this->annotation = $annotation;
+    }
+
+    public function createDoctrineLoaderInstance(): void
+    {
+        $annotation = new AnnotationLoader(new AnnotationReader());
+
+        $annotation->listener(new SampleListener());
+        $annotation->resource(...self::DIRECTORIES);
+
+        $this->annotation = $annotation;
+    }
+
+    public function createAttributeLoaderInstance(): void
+    {
+        $annotation = new AnnotationLoader(80000 <= \PHP_VERSION_ID ? null : new FallbackAttributeReader());
+
+        $annotation->listener(new SampleListener());
+        $annotation->resource(...self::DIRECTORIES);
+
+        $this->annotation = $annotation;
+    }
+
+    /**
+     * @BeforeMethods({"createSelectiveLoaderInstance"})
+     */
+    public function benchOptimisedSelectiveLoad(): void
+    {
+        $this->annotation->load();
+    }
+
+    /**
+     * @BeforeMethods({"createMergedLoaderInstance"})
+     */
+    public function benchOptimisedMergedLoad(): void
+    {
+        $this->annotation->load();
+    }
+
+    /**
+     * @BeforeMethods({"createDoctrineLoaderInstance"})
+     */
+    public function benchOptimisedDoctrineLoad(): void
+    {
+        $this->annotation->load();
+    }
+
+    /**
+     * @BeforeMethods({"createAttributeLoaderInstance"})
+     */
+    public function benchOptimisedAttributeLoad(): void
+    {
+        $this->annotation->load();
+    }
+
+    public function benchSelectiveLoad(): void
+    {
+        $this->createSelectiveLoaderInstance();
+        $this->annotation->load();
+    }
+
+    public function benchMergedLoad(): void
+    {
+        $this->createMergedLoaderInstance();
+        $this->annotation->load();
+    }
+
+    public function benchDoctrineLoad(): void
+    {
+        $this->createDoctrineLoaderInstance();
+        $this->annotation->load();
+    }
+
+    public function benchAttributeLoad(): void
+    {
+        $this->createAttributeLoaderInstance();
+        $this->annotation->load();
+    }
+}
---
+++ tools/benchmark/autoload.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Biurad opensource projects.
+ *
+ * @copyright 2022 Biurad Group (https://biurad.com/)
+ * @license   https://opensource.org/licenses/BSD-3-Clause License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__.'./../../vendor/autoload.php';
+
+\spl_autoload_register(function (string $class): void {
+  $names = \explode('\\', $class);
+
+  if (['Biurad', 'Tests', 'Fixtures'] === [$names[0] ?? null, $names[2] ?? null, $names[3] ?? null]) {
+    $className = \implode('\\', \array_slice($names, 4));
+
+    if ('Annotations' === $names[1]) {
+      if ('annotated_function' === \end($names)) {
+        $className = 'Annotation/function';
+      }
+
+      require __DIR__.'/../../packages/annotations/tests/Fixtures/'.$className.'.php';
+    }
+  }
+});
