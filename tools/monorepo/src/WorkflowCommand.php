a6afbe48db01cd6b4a11f68bd765d34d0e9a75e5
Divine Niiquaye Ibok <divineibok@gmail.com>
2024-02-21 05:44:00 +0000

    Revert "Fixed real path issue (monorepo)"
    This reverts commit 59b6324e289d90a3e537e16245d8cfbc7366c273.

--- tools/monorepo/src/WorkflowCommand.php
+++ tools/monorepo/src/WorkflowCommand.php
@@ -97,8 +97,7 @@ class WorkflowCommand extends Command
     {
         $repositories = [];
         $this->output = new SymfonyStyle($input, $output);
-        $path = \realpath(\rtrim($input->getOption('path') ?? $this->rootPath, '\/'));
-        $config = new Config($path, $input->getOption('cache'), $input->getOption('clean'));
+        $config = new Config(\rtrim($input->getOption('path') ?? $this->rootPath, '\/'), $input->getOption('cache'), $input->getOption('clean'));
         $exclusive = $input->getOption('only');
 
         if (empty($jobWorkers = $config['workers'][$stage = $input->getArgument('job')] ?? [])) {
